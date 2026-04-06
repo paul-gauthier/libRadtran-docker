df2270225107024be5a68f6af22aa5c6e3f2376b
Paul Gauthier <paul@aider.chat>
2026-04-05 21:20:13 -0700

    Revert "fix: run uvspec from libRadtran dir to find REPTRAN data"
    This reverts commit c0e473d9a61a7751e2578c3d161290d29226531c.

--- uvspec_docker.sh
+++ uvspec_docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-host_pwd="$(pwd)"
 exec docker run --rm -i \
-    -v "${host_pwd}:${host_pwd}" \
-    -w /opt/libRadtran-2.0.6 \
+    -v "$(pwd):$(pwd)" \
+    -w "$(pwd)" \
     libradtran:2.0.6 uvspec "$@"
